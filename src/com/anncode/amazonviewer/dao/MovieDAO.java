7b6ee3e987584accd1a611a69a5202df7faa0248
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2022-12-30 10:05:52 -0500

    Conexion a BD local, JDBC y Sentencia SELECT en Java

---
+++ src/com/anncode/amazonviewer/dao/MovieDAO.java
@@ -0,0 +1,46 @@
+package com.anncode.amazonviewer.dao;
+
+import com.anncode.amazonviewer.db.IDBConnection;
+import com.anncode.amazonviewer.model.Movie;
+import  static com.anncode.amazonviewer.db.DataBase.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface MovieDAO extends IDBConnection {
+
+    default Movie setMovieViewed(Movie movie) {
+        return movie;
+    }
+
+    default ArrayList<Movie> read() {
+        ArrayList<Movie> movies = new ArrayList<>();
+        try(Connection connection = connectToDB()){
+            String query = "SELECT * FROM "+ TMOVIE;
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Movie movie = new Movie(
+                        rs.getString(TMOVIE_TITLE),
+                        rs.getString(TMOVIE_GENRE),
+                        rs.getString(TMOVIE_CREATOR),
+                        Integer.valueOf(rs.getString(TMOVIE_DURATION)),
+                        Short.valueOf(rs.getString(TMOVIE_YEAR)));
+
+                movie.setId(Integer.valueOf(rs.getString(TMOVIE_ID)));
+                movies.add(movie);
+
+            }
+        }catch (SQLException e){
+
+        }
+        return movies;
+    }
+
+    private boolean getMovieViewed() {
+        return false;
+    }
+}
